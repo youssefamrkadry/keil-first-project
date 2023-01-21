9c17e4f2589d2298e63d9234604dd05e020d1fa8
Youssef Amr Kadry <youssefamr2008@gmail.com>
2023-01-21 21:16:11 +0200

    change Dio_ChannelType referencing

--- Src/Mcal/Inc/Dio.h
+++ Src/Mcal/Inc/Dio.h
@@ -30,15 +30,15 @@
  *  GLOBAL DATA TYPES AND STRUCTURES
  *********************************************************************************************************************/
 
-// You can perform %1000 to get the masking address and /1000 to get the PortType
+// You can perform %10 to get the bit and /10 to get the PortType
 typedef enum
 {
-    A0 = PORTA*1000 + 1, A1 = PORTA*1000 + 2, A2 = PORTA*1000 + 4, A3 = PORTA*1000 + 8, A4 = PORTA*1000 + 16, A5 = PORTA*1000 + 32, A6 = PORTA*1000 + 64, A7 = PORTA*1000 + 128,
-    B0 = PORTB*1000 + 1, B1 = PORTB*1000 + 2, B2 = PORTB*1000 + 4, B3 = PORTB*1000 + 8, B4 = PORTB*1000 + 16, B5 = PORTB*1000 + 32, B6 = PORTB*1000 + 64, B7 = PORTB*1000 + 128,
-    C0 = PORTC*1000 + 1, C1 = PORTC*1000 + 2, C2 = PORTC*1000 + 4, C3 = PORTC*1000 + 8, C4 = PORTC*1000 + 16, C5 = PORTC*1000 + 32, C6 = PORTC*1000 + 64, C7 = PORTC*1000 + 128,
-    D0 = PORTD*1000 + 1, D1 = PORTD*1000 + 2, D2 = PORTD*1000 + 4, D3 = PORTD*1000 + 8, D4 = PORTD*1000 + 16, D5 = PORTD*1000 + 32, D6 = PORTD*1000 + 64, D7 = PORTD*1000 + 128,
-    E0 = PORTE*1000 + 1, E1 = PORTE*1000 + 2, E2 = PORTE*1000 + 4, E3 = PORTE*1000 + 8, E4 = PORTE*1000 + 16, E5 = PORTE*1000 + 32,
-    F0 = PORTF*1000 + 1, F1 = PORTF*1000 + 2, F2 = PORTF*1000 + 4, F3 = PORTF*1000 + 8, F4 = PORTF*1000 + 16
+    A0 = PORTA*10 + 0, A1 = PORTA*10 + 1, A2 = PORTA*10 + 2, A3 = PORTA*10 + 3, A4 = PORTA*10 + 4, A5 = PORTA*10 + 5, A6 = PORTA*10 + 6, A7 = PORTA*10 + 7,
+    B0 = PORTB*10 + 0, B1 = PORTB*10 + 1, B2 = PORTB*10 + 2, B3 = PORTB*10 + 3, B4 = PORTB*10 + 4, B5 = PORTB*10 + 5, B6 = PORTB*10 + 6, B7 = PORTB*10 + 7,
+    C0 = PORTC*10 + 0, C1 = PORTC*10 + 1, C2 = PORTC*10 + 2, C3 = PORTC*10 + 3, C4 = PORTC*10 + 4, C5 = PORTC*10 + 5, C6 = PORTC*10 + 6, C7 = PORTC*10 + 7,
+    D0 = PORTD*10 + 0, D1 = PORTD*10 + 1, D2 = PORTD*10 + 2, D3 = PORTD*10 + 3, D4 = PORTD*10 + 4, D5 = PORTD*10 + 5, D6 = PORTD*10 + 6, D7 = PORTD*10 + 7,
+    E0 = PORTE*10 + 0, E1 = PORTE*10 + 1, E2 = PORTE*10 + 2, E3 = PORTE*10 + 3, E4 = PORTE*10 + 4, E5 = PORTE*10 + 5,
+    F0 = PORTF*10 + 0, F1 = PORTF*10 + 1, F2 = PORTF*10 + 2, F3 = PORTF*10 + 3, F4 = PORTF*10 + 4
 }Dio_ChannelType;
 
 typedef enum
