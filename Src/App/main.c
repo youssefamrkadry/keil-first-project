04da1d8e838849e386742590dade30123745d98e
Youssef Amr Kadry <youssefamr2008@gmail.com>
2023-01-23 21:37:24 +0200

    Implement Clk driver

--- Src/App/main.c
+++ Src/App/main.c
@@ -1,9 +1,11 @@
 #include "../Mcal/Inc/IntCtrl.h"
 #include "../Mcal/Inc/Dio.h"
 #include "../Mcal/Inc/Port.h"
+#include "../Mcal/Inc/Clk.h"
 
 int main(void)
 {
+    Clk_Init();
     IntCrtl_Init();
     Port_Init();
     Dio_WriteChannel(B6, HIGH);
