7161eb1a352bd5ee3dee6fd4b4f69ccffeef6e6c
Youssef Amr Kadry <youssefamr2008@gmail.com>
2023-01-23 19:37:30 +0200

    added RCC and RCC2 to Mcu_Hw

--- Src/Common/Mcu_Hw.h
+++ Src/Common/Mcu_Hw.h
@@ -43,6 +43,52 @@ typedef union
     INTCTRL_BF B; // Bit
 }INTCTRL_Tag;
 
+typedef struct 
+{
+    uint32 MOSCDIS   :1;
+    uint32           :3;
+    uint32 OSCSRC    :2;
+    uint32 XTAL      :5;
+    uint32 BYPASS    :1;
+    uint32           :1;
+    uint32 PWRDN     :1;
+    uint32           :3;
+    uint32 PWMDIV    :3;
+    uint32 USEPWMDIV :1;
+    uint32           :1;
+    uint32 USESYSDIV :1;
+    uint32 SYSDIV    :4;
+    uint32 ACG       :1;
+    uint32           :4;
+}RCC_BF;
+typedef union 
+{
+    uint32 R; // Register
+    RCC_BF B; // Bit
+}RCC_Tag;
+
+typedef struct 
+{
+    uint32             :4;
+    uint32 OSCSRC2     :3;
+    uint32             :4;
+    uint32 BYPASS2     :1;
+    uint32             :1;
+    uint32 PWRDN2      :1;
+    uint32 USBPWRDN    :1;
+    uint32             :7;
+    uint32 SYSDIV2LSB  :1;
+    uint32 SYSDIV2     :6;
+    uint32             :1;
+    uint32 DIV400      :1;
+    uint32 USERCC2     :1;
+}RCC2_BF;
+typedef union 
+{
+    uint32 R; // Register
+    RCC2_BF B; // Bit
+}RCC2_Tag;
+
 
 
 /**********************************************************************************************************************
@@ -56,6 +102,8 @@ typedef union
 #define GPIO_BASE_ADDRESS_AHB                  0x40058000
 
 #define SYS_CTRL_BASE_ADDRESS                  0x400FE000
+#define RCC                                    *((volatile RCC_Tag*)(SYS_CTRL_BASE_ADDRESS+0x060))
+#define RCC2                                   *((volatile RCC2_Tag*)(SYS_CTRL_BASE_ADDRESS+0x070))
 #define RCGCGPIO                               *((volatile uint32*)(SYS_CTRL_BASE_ADDRESS+0x608))
 
 /**********************************************************************************************************************
