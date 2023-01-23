707977d2dd9a71848605e449cda666db307aabd1
Youssef Amr Kadry <youssefamr2008@gmail.com>
2023-01-23 22:27:25 +0200

    Implement SysTick.c

--- Src/Mcal/SysTick.c
+++ Src/Mcal/SysTick.c
@@ -1,48 +1,48 @@
 /**********************************************************************************************************************
-
  *  FILE DESCRIPTION
- *  -------------------------------------------------------------------------------------------------------------------
- *         File:  SysTick.h
- *       Module:  SysTick
+ *  -----------------------------------------------------------------------------------------------------------------*/
+/**        \file  SysTick.c
+ *        \brief  SysTick timer driver
  *
- *  Description:  header file for SysTick Module    
- *  
+ *      \details  The criver configures the systick timer and utilizes its functions during runtime
+ *
  *********************************************************************************************************************/
-#ifndef SYSTICK_H
-#define SYSTICK_H
 
 /**********************************************************************************************************************
- * INCLUDES
+ *  INCLUDES
  *********************************************************************************************************************/
-#include "../../Common/Std_Types.h"
+#include "Inc/SysTick.h"
+#include "../Common/Mcu_Hw.h"
+#include "../Config/Clk_Cfg.h"
 
 /**********************************************************************************************************************
- *  GLOBAL CONSTANT MACROS
- *********************************************************************************************************************/
-
-
-/**********************************************************************************************************************
- *  GLOBAL FUNCTION MACROS
- *********************************************************************************************************************/
-
+*  LOCAL MACROS CONSTANT\FUNCTION
+*********************************************************************************************************************/	
 
 /**********************************************************************************************************************
- *  GLOBAL DATA TYPES AND STRUCTURES
+ *  LOCAL DATA 
  *********************************************************************************************************************/
-typedef void(*cb_type)(void);
+cb_type callback_ptr = NULL_PTR;
+/**********************************************************************************************************************
+ *  GLOBAL DATA
+ *********************************************************************************************************************/
 
 /**********************************************************************************************************************
- *  GLOBAL DATA PROTOTYPES
+ *  LOCAL FUNCTION PROTOTYPES
  *********************************************************************************************************************/
 
- 
 /**********************************************************************************************************************
- *  GLOBAL FUNCTION PROTOTYPES
+ *  LOCAL FUNCTIONS
  *********************************************************************************************************************/
- 
+
+/**********************************************************************************************************************
+ *  GLOBAL FUNCTIONS
+ *********************************************************************************************************************/
+
+
 /******************************************************************************
 * \Syntax          : void SysTick_Start(void)                                      
-* \Description     : start SysTick Timer                              
+* \Description     : initialize SysTick Timer                              
 *                                                                             
 * \Sync\Async      : Synchronous                                               
 * \Reentrancy      : Non Reentrant                                             
@@ -50,7 +50,12 @@ typedef void(*cb_type)(void);
 * \Parameters (out): None                                                      
 * \Return value:   : None
 *******************************************************************************/
-void SysTick_Start(void);
+void SysTick_Start(void)
+{
+    STCTRL |= (1<<2); // select system clock for clock source
+    STCTRL |= (1<<1); // enable SysTick interrupt
+    STCTRL |= (1<<0); // enable timer in multi-shot mode
+}
 
 /******************************************************************************
 * \Syntax          : void SysTick_InterruptCountdown(uint32 countdown_ms)
@@ -63,7 +68,43 @@ void SysTick_Start(void);
 * \Parameters (out): None                                                      
 * \Return value:   : None
 *******************************************************************************/
-void SysTick_InterruptCountdown(uint32 countdown_ms);
+void SysTick_InterruptCountdown(uint32 countdown_ms)
+{
+    uint32 reload_value;
+
+    switch (CLK_XTAL)
+    {
+    case XTAL_5:
+        reload_value = (500*countdown_ms) - 1;
+        break;
+    case XTAL_6:
+        reload_value = (600*countdown_ms) - 1;
+        break;
+    case XTAL_8:
+        reload_value = (800*countdown_ms) - 1;
+        break;
+    case XTAL_10:
+        reload_value = (1000*countdown_ms) - 1;
+        break;
+    case XTAL_12:
+        reload_value = (1200*countdown_ms) - 1;
+        break;
+    case XTAL_16:
+        reload_value = (1600*countdown_ms) - 1;
+        break;
+    default:
+        reload_value = 0;
+        break;
+    }
+
+    STRELOAD &= ~(0x00FFFFFF);
+    if (reload_value <= 0x00FFFFFF)
+    {
+        STRELOAD |= reload_value;
+    }
+    STCURRENT &= ~(0x00FFFFFF);
+
+}
 
 /******************************************************************************
 * \Syntax          : void SysTick_RegisterInterruptCallback(cb_type ptr)
@@ -75,10 +116,32 @@ void SysTick_InterruptCountdown(uint32 countdown_ms);
 * \Parameters (out): None                                                      
 * \Return value:   : None
 *******************************************************************************/
-void SysTick_RegisterInterruptCallback(cb_type ptr);
- 
-#endif  /* IntCrtl_H */
+void SysTick_RegisterInterruptCallback(cb_type *ptr)
+{
+    if (ptr != NULL_PTR)
+    {
+        callback_ptr = ptr;
+    }
+}
+
+/******************************************************************************
+* \Syntax          : void SysTick_Handler()
+* \Description     : ISR for SysTick Timer
+*                                                                             
+* \Sync\Async      : Synchronous                                               
+* \Reentrancy      : Non Reentrant                                             
+* \Parameters (in) : None                     
+* \Parameters (out): None                                                      
+* \Return value:   : None
+*******************************************************************************/
+void SysTick_Handler()
+{
+    if (callback_ptr != NULL_PTR)
+    {
+        callback_ptr();
+    }
+}
 
 /**********************************************************************************************************************
- *  END OF FILE: IntCrtl.h
+ *  END OF FILE: SysTick.c
  *********************************************************************************************************************/
